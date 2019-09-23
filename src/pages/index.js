b3c0918410364ff38070b13362543654da72fcf4
Kensuke Shibata <kensukeshibata@gmail.com>
2019-09-23 17:42:45 +1000

    edit content

--- src/pages/index.js
+++ src/pages/index.js
@@ -141,46 +141,61 @@ const IndexPage = () => (
             <img src={pic8} alt="" />
           </a>
         </div>
+        <h3>Work History</h3>
         <p>
-          Work History I started my career as a Desktop Engineer in March 2014s
-          which allowed me to operate on Windows and Unix platforms. In
-          September 2014, I was involved in a large-scale, high-demanding
-          software release as a QA tester of the very first online charging
-          system in Japan that now has 10 million subscribers with NOKIA. This
-          online charging software system, developed by REDKNEE India and
+          I started my career as a <span>Desktop Engineer</span> in{' '}
+          <span>March 2014</span>s which allowed me to operate on Windows and
+          Unix platforms. In <span>September 2014</span>, I was involved in a
+          large-scale, high-demanding software release as a{' '}
+          <span>QA tester</span> of the very first online charging system in
+          Japan that now has 10 million subscribers with <span>NOKIA</span>.
+          This online charging software system, developed by REDKNEE India and
           Germany and NOKIA Japan, needed testing for meeting quality
           requirements and KDDI Japan carrier’s design requirements. To do the
           testing from the Nokia’s side, I learned the testing know-how from the
           development engineer and successfully implemented my tasks. During
-          that hands-on experience, I became very familiar with the Unix
-          operation, bash, and Windows server. I investigated the problems based
-          on the log and analyzed the packet using Wireshark. I also learned
-          testing procedures from the German and Indian test engineers through
-          which experience I became a better communicator, able to discuss
-          roadblocks and alternative solutions with my colleagues on the
-          international team. In August 2015, when OCS went live, I was promoted
-          to the role of Level 2 Customer Support Engineer. I was responsible
-          for solving tickets from our customers in a timely manner. Being a
-          Level 2 engineer in the environment where quality control standards
-          were very high, taught me the best way to investigate customer
-          problems, test and reproduce issues in the lab and apply fixed
-          procedures on the online software which could potentially impact 10
-          million subscribers. I have experienced firsthand the commercial
-          system upgrade and realized how a live system should operate, being
-          extremely careful executing some of the commands. I was also involved
-          in developing the upgrade procedure documentation for the commercial
-          use and the on-site upgrades. Education After high shool, I studied
-          film production at De Anza College which located in the Bay Area,
-          California. I completed Intro to Programming Nanodegree at Udacity in
-          2016 and completed Full Stack Web Developer Nanodegree in 2018.
-          Currently, I started 6 months Programming BootCamp at Coder Academy in
-          Sydney. I love. I love my spare spend my time in the ocean and get
-          salty. I love to play backgammon with frinds while drinking a mocha at
-          cafe. I had a blast time in New Zealand and Australia (working
-          holiday) where I could surf one of the best surf spot in the world. I
-          also like the part that constatnly try to improve my surf skills.
-          Surfing is hard so as Programming. I will never master everthing in my
-          life time but that dones't stop me to keep improving.
+          that hands-on experience, I became very familiar with the{' '}
+          <span>Unix operation, bash</span>, and Windows server. I investigated
+          the problems based on the log and analyzed the packet using{' '}
+          <span>Wireshark</span>. I also learned testing procedures from the
+          German and Indian test engineers through which experience I became a
+          better communicator, able to discuss roadblocks and alternative
+          solutions with my colleagues on the <span>international team</span>.
+          <br />
+          In <span>August 2015</span>, when OCS went live, I was promoted to the
+          role of <span>Level 2 Customer Support Engineer</span>. I was
+          responsible for solving tickets from our customers in a timely manner.
+          Being a Level 2 engineer in the environment where quality control
+          standards were very high, taught me the best way to investigate
+          customer problems, test and reproduce issues in the lab and apply
+          fixed procedures on the online software which could potentially impact{' '}
+          <span>10 million subscribers</span>. I have experienced firsthand the
+          commercial system upgrade and realized how a live system should
+          operate, being extremely careful executing some of the commands. I was
+          also involved in developing the upgrade procedure documentation for
+          the commercial use and the on-site upgrades.
+        </p>
+        <h3>Education</h3>
+        <p>
+          After high shool, I studied film production at{' '}
+          <span>De Anza College</span> which located in the{' '}
+          <span>Bay Area, California</span>. I completed Intro to Programming
+          Nanodegree at <span>Udacity</span> in 2016 and completed{' '}
+          <span>Full Stack Web Developer Nanodegree</span> in 2018. Currently, I
+          started 6 months Programming BootCamp at <span>Coder Academy</span> in
+          Sydney.
+        </p>
+        <h3>I love.</h3>
+        <p>
+          I love my spare spend my time in the <span>ocean</span> and get salty.
+          I love to play <span>backgammon</span> with frinds while drinking a{' '}
+          <span>mocha</span> at cafe. I had a blast time in{' '}
+          <span>New Zealand</span> and <span>Australia</span> (working holiday)
+          where I could <span>surf</span> one of the best surf spot in the
+          world. I also like the part that constatnly try to improve my surf
+          skills. Surfing is hard so as <span>Programming</span>. I will never
+          master everthing in my life time but that dones't stop me to{' '}
+          <span>keep improving</span>.
         </p>
       </section>
 
@@ -190,13 +205,7 @@ const IndexPage = () => (
             <h2>Contact</h2>
           </header>
 
-          <p>
-            The element of time, sem ante ullamcorper dolor nulla quam placerat
-            viverra environment is not with our customers. Free makeup and skirt
-            until the mouse. Japan this innovative and ultricies carton salad
-            clinical ridiculous now passes from enhanced. Mauris pot innovative
-            care for my pain.
-          </p>
+          <p>Have questions? Grab some coffee? Message me!!</p>
 
           <form method="post" action="#">
             <div className="row">
